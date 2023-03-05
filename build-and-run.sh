cb62025c68f436e8480ea5b2bff22887193a7aaa
hakbar0 <haseeb2000@hotmail.com>
2023-03-05 13:07:23 +0000

    updated bash file with new names

--- build-and-run.sh
+++ build-and-run.sh
@@ -8,8 +8,8 @@ docker stop test-nest
 docker rm test-nest
 
 # Start a new Postgres container with the database "auth" if it doesn't exist
-docker run --name pay -p 5432:5432 -e POSTGRES_PASSWORD=root -d postgres
-docker exec -it pay bash -c "psql -U postgres -c 'CREATE DATABASE IF NOT EXISTS auth;'"
+docker run --name test -p 5432:5432 -e POSTGRES_PASSWORD=root -d postgres
+docker exec -it test bash -c "psql -U postgres -c 'CREATE DATABASE IF NOT EXISTS auth;'"
 
 # Start a new container using the image and publish port 3000
-docker run --name test-nest -p 3000:3000 --link pay:test-nest -e POSTGRES_HOST=pay -e POSTGRES_PORT=5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root -e POSTGRES_DB=auth test-nest
+docker run --name test-nest -p 3000:3000 --link test:test-nest -e POSTGRES_HOST=test -e POSTGRES_PORT=5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root -e POSTGRES_DB=auth test-nest
